e21be3ebb995ddcff1bc6000a8e11d72268de626
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-10-07 16:22:03 -0300

    aula 3

--- selectionSort.js
+++ selectionSort.js
@@ -6,10 +6,7 @@ for (let atual = 0; atual < livros.length - 1; atual++) {
   let menor = menorValor(livros, atual)
 
   let livroAtual = livros[atual];
-  console.log('posição atual', atual)
-  console.log('livro atual', livros[atual])
   let livroMenorPreco = livros[menor];
-  console.log('livro menor preço', livros[menor])
 
   livros[atual] = livroMenorPreco
   livros[menor] = livroAtual
